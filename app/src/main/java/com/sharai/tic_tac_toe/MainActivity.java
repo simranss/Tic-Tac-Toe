1629bd8e6b779a72f79fc4caeb8497fad9b6753a
Simran Sharma <simran.s.sharma00@gmail.com>
2021-04-04 19:06:08 +0530

    Game has playAgain feature and it can tell if it's a draw

--- app/src/main/java/com/sharai/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/sharai/tic_tac_toe/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     int activePlayer = 0;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    boolean gameActive = true;
 
     GridLayout gridLayout;
     TextView resultTextView;
@@ -39,36 +40,75 @@ public class MainActivity extends AppCompatActivity {
         playAgainButton = findViewById(R.id.play_again_button);
 
         resultTextView.setVisibility(View.GONE);
+        playAgainButton.setVisibility(View.GONE);
+
+        playAgainButton.setOnClickListener(v -> {
+            gameActive = true;
+            playAgainButton.setVisibility(View.GONE);
+            resultTextView.setVisibility(View.GONE);
+
+            int childCount = gridLayout.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                ImageView piece = (ImageView) gridLayout.getChildAt(i);
+                piece.setImageDrawable(null);
+            }
+
+            activePlayer = 0;
+            gameState = new int[]{2, 2, 2, 2, 2, 2, 2, 2, 2};
+            gameActive = true;
+        });
     }
 
     public void dropIn(View view) {
         ImageView piece = (ImageView) view;
 
         int tappedPiece = Integer.parseInt(piece.getTag().toString());
-        gameState[tappedPiece] = activePlayer;
 
-        if (activePlayer == 0) {
-            piece.setImageResource(R.drawable.x);
-            activePlayer = 1;
-        } else if (activePlayer == 1) {
-            piece.setImageResource(R.drawable.o);
-            activePlayer = 0;
-        }
-        fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        piece.startAnimation(fadeIn);
+        if (gameState[tappedPiece] == 2 && gameActive) {
+            gameState[tappedPiece] = activePlayer;
 
-        for (int[] winningPosition : winningPositions) {
-            Log.d(TAG, "winningPosition: " + Arrays.toString(winningPosition));
-            Log.d(TAG, "gameState[winningPosition[0]]: " + gameState[winningPosition[0]]);
-            Log.d(TAG, "gameState[winningPosition[1]]: " + gameState[winningPosition[1]]);
-            Log.d(TAG, "gameState[winningPosition[2]]: " + gameState[winningPosition[2]]);
+            if (activePlayer == 0) {
+                piece.setImageResource(R.drawable.x);
+                activePlayer = 1;
+            } else if (activePlayer == 1) {
+                piece.setImageResource(R.drawable.o);
+                activePlayer = 0;
+            }
+            fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+            piece.startAnimation(fadeIn);
 
-            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
-                // someone has won!
-                Log.d(TAG, "someone has won!");
-                gridLayout.setEnabled(false);
+            for (int[] winningPosition : winningPositions) {
+                Log.d(TAG, "winningPosition: " + Arrays.toString(winningPosition));
+                Log.d(TAG, "gameState[winningPosition[0]]: " + gameState[winningPosition[0]]);
+                Log.d(TAG, "gameState[winningPosition[1]]: " + gameState[winningPosition[1]]);
+                Log.d(TAG, "gameState[winningPosition[2]]: " + gameState[winningPosition[2]]);
+
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                    // someone has won!
+                    Log.d(TAG, "someone has won!");
+                    gameActive = false;
+                    resultTextView.setVisibility(View.VISIBLE);
+                    playAgainButton.setVisibility(View.VISIBLE);
+                    resultTextView.setText(getString(R.string.resultText, (gameState[winningPosition[0]] + 1)));
+                    return;
+                }
+            }
+
+            boolean draw = false;
+            for (int value : gameState) {
+                if (value == 2) {
+                    draw = false;
+                    break;
+                } else {
+                    draw = true;
+                }
+            }
+            if (draw) {
+                Log.d(TAG, "it is a draw");
+                gameActive = false;
                 resultTextView.setVisibility(View.VISIBLE);
-                resultTextView.setText(getString(R.string.resultText, (gameState[winningPosition[0]] + 1)));
+                playAgainButton.setVisibility(View.VISIBLE);
+                resultTextView.setText(getString(R.string.resultDraw));
             }
         }
     }
